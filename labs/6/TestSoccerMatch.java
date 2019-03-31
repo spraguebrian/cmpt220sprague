1afc7223e5dc56a2d5d88a6b065e074f2fd29669
Juan Arias <juan.arias@marist.edu>
2019-03-31 18:01:32 -0400

    Feedback

--- labs/6/TestSoccerMatch.java
+++ labs/6/TestSoccerMatch.java
@@ -1,8 +1,9 @@
 import java.util.Date;
+// JA: This is not able to show the functionality
 public class TestSoccerMatch {
 
 	public static void main(String[] args) {
-		SoccerMatch s1 = new SoccerMatch(3, 4, "red", "blue");
+		SoccerMatch s1 = new SoccerMatch(new Date(), null, "red", "blue");
 
 	}
 
